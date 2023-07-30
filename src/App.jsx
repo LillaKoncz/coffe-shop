71430fb5c08dc7fadf663e5e4701a94a80a1f41c
LillaKoncz <koncz.lilla92@gmail.com>
2023-07-30 06:12:18 +0200

    display hello word

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h2>Hello World shold be display </h2>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
