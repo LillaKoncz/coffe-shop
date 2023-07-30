cbed7f5e0a473c7dfaa84475b84b3beed86f8533
LillaKoncz <koncz.lilla92@gmail.com>
2023-07-30 06:13:45 +0200

    hello world display

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
   return (
     <>
       <div>
-        <h2>Hello World shold be display </h2>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Hello World shold be display </h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
